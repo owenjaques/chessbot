e47bf7242de9153495735b6b2fa6f1ebe5fb9693
Owen Jaques <owenjaques2000@gmail.com>
2023-03-09 17:15:43 -0800

    Changed to using just one model instead of one per color.

--- src/chessbot/chessbot.py
+++ src/chessbot/chessbot.py
@@ -1,12 +1,14 @@
 import random
 import numpy as np
+import chess
 from .model_input import ModelInput
 
 class ChessBot:
-    def __init__(self, model, exploration_rate=0.0):
+    def __init__(self, model, color, exploration_rate=0.0):
         self.model = model
         self.moves_made = []
         self.exploration_rate = exploration_rate
+        self.color = color
         
     def move(self, board):
         moves, model_inputs = self.get_potential_moves(board)
@@ -35,5 +37,5 @@ class ChessBot:
     
     def get_best_move(self, moves, model_inputs):
         predictions = self.model.predict(model_inputs)
-        move_index = predictions.argmax()
+        move_index = predictions.argmax() if self.color == chess.WHITE else predictions.argmin()
         return model_inputs[move_index], moves[move_index]
