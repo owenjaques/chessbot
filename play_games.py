b0755822f837d614a2b4c2b871d7deb9d27395f7
Nicholas Wurzer <nicholaswurzer@Nicholass-MacBook-Pro.local>
2023-03-17 16:20:26 -0700

    Visualize Elo as Games Played
    Simulated games and Plotted Elo.  Randomized the game order for more accurate Elo calculation.

--- play_games.py
+++ play_games.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+
+#This class is meant to estimate the elo of our chess engine.
 import chess
 import chess.svg
 from stockfish import Stockfish
 import typing
+import random
 #This line should be replaced with whatever model we want to test.
 #The model must impliment a function called get_next_move().
 import a_really_bad_chess_engine
@@ -15,12 +18,12 @@ def get_winner_of_game(stockfish_engine, ML_engine):
     stockfish_engine.set_position([])
     board = chess.Board()
     turn = chess.WHITE
-        #check if the game is over
-        #must update both board and stockfish with the move
+    #check if the game is over
     while(not outcome):
         if(turn == chess.WHITE):
             #get stockfish's move
             move = stockfish_engine.get_best_move()
+            #must update both board and stockfish with the move
             board.push(chess.Move.from_uci(move))
             stockfish_engine.make_moves_from_current_position([move])
             turn = not turn
@@ -55,9 +58,13 @@ def main():
     #change to wherever you've installed stockfish
     stockfish = Stockfish(path = "/usr/local/bin/stockfish", parameters = {"UCI_LimitStrength" : "true"})
     games_won = []
+    #starting elo
     our_elo = 1500
+    #list of elos which will set the stokcfish engine's elo for each game
+    elos = list(range(1350, 2851, 20))
+    random.shuffle(elos)
 
-    for elo in range(1350, 2851, 50):
+    for elo in elos:
         stockfish.update_engine_parameters({"UCI_Elo" : elo})
         winner = get_winner_of_game(stockfish, a_really_bad_chess_engine).winner
         games_won += [(elo, not winner)]
@@ -65,6 +72,7 @@ def main():
             elo, our_elo = calculate_elo(elo, our_elo)
         else:
             our_elo, elo = calculate_elo(our_elo, elo)
+
     print(games_won)
     print(our_elo)
 
