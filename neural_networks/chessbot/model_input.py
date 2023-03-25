8bd882b891e3d4a360412be0ca4746cf04aaa17d
Owen Jaques <owenjaques2000@gmail.com>
2023-03-24 17:29:17 -0700

    Updated model_input to take a board in the input function so the object can be reused.

--- neural_networks/chessbot/model_input.py
+++ neural_networks/chessbot/model_input.py
@@ -18,21 +18,16 @@ class ModelInput:
     potential_game_outcome = np.empty(1)
     next_to_move = np.empty(1)
 
-    def __init__(self, board):
-        self.board = board
-        self.parse_board()
-        self.parse_castling_rights()
-        self.parse_potential_game_outcome()
-        self.get_next_to_move()
-        self.atk_lst()
+    def __init__(self, input_type='positions'):
+        self.input_type = input_type
         
-    def parse_board(self):
+    def parse_board(self, board):
         rook_index = np.zeros(2, dtype=int)
         knight_index = np.zeros(2, dtype=int)
         bishop_index = np.zeros(2, dtype=int)
         pawn_index = np.zeros(2, dtype=int)
 
-        piece_map = self.board.piece_map()
+        piece_map = board.piece_map()
         for square, piece in piece_map.items():
             color = int(piece.color)
             rank = chess.square_rank(square)
@@ -55,19 +50,13 @@ class ModelInput:
                 self.pawns[color][pawn_index[color]] = [1, rank, file]
                 pawn_index[color] += 1
 
-        # sort per piece type according to the rank then the file if rank is the same
-        # self.rooks = np.sort(self.rooks, axis=1)
-        # self.knights = np.sort(self.knights, axis=1)
-        # self.bishops = np.sort(self.bishops, axis=1)
-        # self.pawns = np.sort(self.pawns, axis=1)
-
         # Normalise positions between 0 and 1 (0 will represent the position of a piece which has been captured)
-        self.rooks[:, :, 1:] = (self.rooks[:, :, 1:] + 1) / 8
-        self.knights[:, :, 1:] = (self.knights[:, :, 1:] + 1) / 8
-        self.bishops[:, :, 1:] = (self.bishops[:, :, 1:] + 1) / 8
-        self.queen[:, 1:] = (self.queen[:, 1:] + 1) / 8
-        self.king[:, 1:] = (self.king[:, 1:] + 1) / 8
-        self.pawns[:, :, 1:] = (self.pawns[:, :, 1:] + 1) / 8
+        self.rooks[:, :, 1:] = (self.rooks[:, :, 1:]) / 7
+        self.knights[:, :, 1:] = (self.knights[:, :, 1:]) / 7
+        self.bishops[:, :, 1:] = (self.bishops[:, :, 1:]) / 7
+        self.queen[:, 1:] = (self.queen[:, 1:]) / 7
+        self.king[:, 1:] = (self.king[:, 1:]) / 7
+        self.pawns[:, :, 1:] = (self.pawns[:, :, 1:]) / 7
 
         # Normalise existence of pieces to be either 0 or 1
         self.rooks[self.rooks == -1] = 0
@@ -77,37 +66,37 @@ class ModelInput:
         self.king[self.king == -1] = 0
         self.pawns[self.pawns == -1] = 0
 
-    def parse_castling_rights(self):
-        self.castling_rights[0] = int(self.board.has_kingside_castling_rights(chess.WHITE))
-        self.castling_rights[1] = int(self.board.has_queenside_castling_rights(chess.WHITE))
-        self.castling_rights[2] = int(self.board.has_kingside_castling_rights(chess.BLACK))
-        self.castling_rights[3] = int(self.board.has_queenside_castling_rights(chess.BLACK))
+    def parse_castling_rights(self, board):
+        self.castling_rights[0] = int(board.has_kingside_castling_rights(chess.WHITE))
+        self.castling_rights[1] = int(board.has_queenside_castling_rights(chess.WHITE))
+        self.castling_rights[2] = int(board.has_kingside_castling_rights(chess.BLACK))
+        self.castling_rights[3] = int(board.has_queenside_castling_rights(chess.BLACK))
 
-    def parse_potential_game_outcome(self):
-        game_outcome = self.board.outcome(claim_draw=True)
+    def parse_potential_game_outcome(self, board):
+        game_outcome = board.outcome(claim_draw=True)
         if game_outcome is None:
             self.potential_game_outcome[0] = 0.66
         else:
-            if (game_outcome.winner == chess.WHITE and self.board.ply() % 2 == 1) or (game_outcome.winner == chess.BLACK and self.board.ply() % 2 == 0):
+            if (game_outcome.winner == chess.WHITE and board.ply() % 2 == 1) or (game_outcome.winner == chess.BLACK and board.ply() % 2 == 0):
                 # win
                 self.potential_game_outcome[0] = 1
-            elif (game_outcome.winner == chess.WHITE and self.board.ply() % 2 == 0) or (game_outcome.winner == chess.BLACK and self.board.ply() % 2 == 1):
+            elif (game_outcome.winner == chess.WHITE and board.ply() % 2 == 0) or (game_outcome.winner == chess.BLACK and board.ply() % 2 == 1):
                 # loss
                 self.potential_game_outcome[0] = 0
             else:
                 # draw
                 self.potential_game_outcome[0] = 0.33
 
-    def get_next_to_move(self):
-        self.next_to_move[0] = 0 if self.board.turn else 1
+    def get_next_to_move(self, board):
+        self.next_to_move[0] = 0 if board.turn else 1
 
-    def atk_lst(self):
+    def atk_lst(self, board):
         white_atk = chess.SquareSet()
         black_atk = chess.SquareSet()
-        for attacker in chess.SquareSet(self.board.occupied_co[chess.WHITE]):
-            white_atk |= self.board.attacks(attacker)
-        for attacker in chess.SquareSet(self.board.occupied_co[chess.BLACK]):
-            black_atk |= self.board.attacks(attacker)
+        for attacker in chess.SquareSet(board.occupied_co[chess.WHITE]):
+            white_atk |= board.attacks(attacker)
+        for attacker in chess.SquareSet(board.occupied_co[chess.BLACK]):
+            black_atk |= board.attacks(attacker)
 
         for i in list(white_atk):
             self.attacks[i] += (1/3)
@@ -115,19 +104,18 @@ class ModelInput:
         for i in list(black_atk):
             self.attacks[i] += (2/3)
         
-    def get_input(self):
-        return np.concatenate([
-            self.rooks.flatten(),
-            self.knights.flatten(),
-            self.bishops.flatten(),
-            self.queen.flatten(),
-            self.king.flatten(),
-            self.pawns.flatten(),
-            self.castling_rights,
-            self.potential_game_outcome,
-            # self.attacks,
-            self.next_to_move
-        ])
+    def get_input(self, board):
+        if self.input_type == 'positions':
+            self.parse_board(board)
+
+            return np.concatenate([
+                self.rooks.flatten(),
+                self.knights.flatten(),
+                self.bishops.flatten(),
+                self.queen.flatten(),
+                self.king.flatten(),
+                self.pawns.flatten()
+            ])
     
 class SimpleModelInput:
     def __init__(self, board):
@@ -167,6 +155,6 @@ class SimpleModelInput:
 if __name__ == '__main__':
     board = chess.Board()
     board.push(chess.Move.from_uci('e2e4'))
-    model_input = ModelInput(board).get_input()
-    print(model_input[:,:,0])
+    model_input = ModelInput().get_input(board)
+    print(model_input)
     print(model_input.shape)
