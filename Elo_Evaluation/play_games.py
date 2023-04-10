70323c7402a81b1a02e22b11f6613587bb9daf05
Kayisn <125847120+Kayisn@users.noreply.github.com>
2023-04-10 14:03:51 -0700

    fixed color
    Engine set to black

--- Elo_Evaluation/play_games.py
+++ Elo_Evaluation/play_games.py
@@ -10,9 +10,12 @@ import random
 #The model must impliment a function called get_next_move().
 #import a_really_bad_chess_engine
 
+from IPython.display import clear_output
+from IPython.display import display
+
 
 #move must be guaranteed to be a legal move
-def get_winner_of_game(stockfish_engine, ML_engine):
+def get_winner_of_game(stockfish_engine, ML_engine, display_board = False):
     #initialize the board
     outcome = False
     stockfish_engine.set_position([])
@@ -37,10 +40,13 @@ def get_winner_of_game(stockfish_engine, ML_engine):
             board.push(move)
             stockfish_engine.make_moves_from_current_position([move])
             turn = not turn
+
             ##uncomment these lines to see chess board in browser - must refresh browser to see updated results...chessboard class may be better option.
             #with open("chess_display.html", "w") as chess_display:
             #    chess_display.write(chess.svg.board(board, size = 500))
-            
+        if(display_board):
+            clear_output(wait=True)
+            display(board)
         outcome = board.outcome()
     #If the winner is True, then the stockfish engine won.
     print(outcome)
@@ -54,18 +60,18 @@ def calculate_elo( winner, loser, k = 32) -> typing.Tuple[int, int]:
     new_loser = loser + k * (0 - expected_loser)
     return (new_winner, new_loser)
 
-def run_elo_evaluation(our_agent, stockfish_opponent):
+def run_elo_evaluation(our_agent, stockfish_opponent, display_board = False):
     #change to wherever you've installed stockfish
     games_won = []
     #starting elo
     our_elo = 1500
     #list of elos which will set the stokcfish engine's elo for each game
-    elos = list(range(1350, 2851, 20))
+    elos = list(range(1350, 1501, 50))
     random.shuffle(elos)
 
     for elo in elos:
         stockfish_opponent.update_engine_parameters({"UCI_Elo" : elo})
-        winner = get_winner_of_game(stockfish_opponent, our_agent).winner
+        winner = get_winner_of_game(stockfish_opponent, our_agent, display_board).winner
         games_won += [(elo, not winner)]
         if winner:
             elo, our_elo = calculate_elo(elo, our_elo)
